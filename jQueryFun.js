654a55f5af3d878cd35d9419a1eafbb7b2cc1d22
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-11-26 02:16:48 -0700

    Initial Commit

---
+++ jQueryFun.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(() => {
+
+    $('.shoe-details').show();
+
+    $('.more-details-button').on('click', event => {
+
+    });
+
+    $('.shoe-details li').on('click', event => {
+        $(event.currentTarget).addClass('active');
+        $(event.currentTarget).siblings().removeClass('active');
+    });
+
+
+
+    ///////////////////////////////////////////
+
+    $('.login-button').on('click', () => {
+        $('.login-form').slideToggle();
+        $('.login-button').toggleClass('button-active');
+    });
+
+    $('.login-form').on('mouseleave', () => {
+        $('.login-form').slideToggle();
+        $('.login-button').toggleClass('button-active');
+    });
+
+
+    $('.product-photo').on('mouseenter', () => {
+        $(this).addClass('photo-active');
+    }).on('mouseleave', function () {
+        $(this).removeClass('photo-active');
+    });
+
+    $('.menu-button').on('click', () => {
+        $('.menu-button').toggleClass('button-active');
+        $('.nav-menu').slideToggle();
+    });
+
+    $('.nav-menu').on('mouseleave', () => {
+        $('.menu-button').toggleClass('button-active');
+        $('.nav-menu').slideToggle();
+    });
+
+})
